bbcd28134ecfb1682bfafa0b706286beea91effb
writeriks <emirhaktan@gmail.com>
2023-10-03 15:13:05 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export const Home = ({ dbUser }: { dbUser: User }) => {
     </>
   );
 };
-
+// test
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps({ req, res }) {
     const session = await getSession(req, res);
